84b9b51bc292a5d3997e339accc0d9f977c62542
Yanuar Eko M <yanuar.mugianto@jawdat.com>
2022-01-20 03:11:03 +0700

    delete print and unecessary config in config.yml.example

--- modules/helper.py
+++ modules/helper.py
@@ -1,21 +1,18 @@
-def non_empty_string(s) -> str or None:
-    """
-    Used by reqparse module, to check weather the value is empty string or not
-    :param s:
-    :return: return string if not error
+def non_empty_string(s):
+    """Used by reqparse module, to check weather the value is empty string or not
+    :param s: any
+    :return: return the value without modification
     """
     if not type(s) == str:
         raise ValueError("Must be string")
     if not s:
         raise ValueError("Must not be empty string")
-    print('called!')
     return s
 
 
-def extract_arguments(args: dict):
-    """
-    To extract arguments send from client
-    :param args:
-    :return:
+def extract_arguments(args: dict) -> tuple:
+    """To extract arguments send from client
+    :param args: result from parser.parse_args()
+    :return: tuple of values from args
     """
     return tuple([val for val in args.values()])
